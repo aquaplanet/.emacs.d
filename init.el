c9220ecc46ebab4beb517e368ae407df498ba2cb
Anders Andersson <git@panic.nu>
2016-12-15 21:54:35 +0100

    Added use-package and magit. init.el
    init.el is automatically changing (going to mention it all the time
    now).

--- init.el
+++ init.el
@@ -61,3 +61,15 @@
     (my-tangle-config-org orgfile elfile))
   (load-file elfile))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
